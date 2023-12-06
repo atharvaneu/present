e7631ace40cb804e5e8d957a1b4deede6718a9b6
Govindrm7 <govindrmdhl8@gmail.com>
2023-12-06 12:33:55 -0500

    changed some UI

--- client/src/pages/contactus/index.tsx
+++ client/src/pages/contactus/index.tsx
@@ -11,32 +11,41 @@ import {
 
 function ContactUs() {
   return (
-    <Box p="4"  backgroundColor="black" color="white">
-      <Heading as="h1" mb="4">
-        Contact Us
-      </Heading>
-      <Box>
-        <Heading as="h2" size="md" mb="2">
-          Our Team
+    <Box
+      p="4"
+      backgroundColor="black"
+      color="white"
+      minHeight="100vh" // Ensures the content takes at least the full viewport height
+      display="flex"
+      flexDirection="column"
+    >
+      <Box flex="1"> {/* This Box will hold the main content */}
+        <Heading as="h1" mb="4">
+          Contact Us
         </Heading>
-        <List styleType="none" ml="0" mb="4">
-          <ListItem>
-            <strong>Govind</strong>:{' '}
-            <Link href="mailto:govind@example.com">govind@example.com</Link>
-          </ListItem>
-          <ListItem>
-            <strong>Atharv</strong>:{' '}
-            <Link href="mailto:atharv@example.com">atharv@example.com</Link>
-          </ListItem>
-          <ListItem>
-            <strong>Gaurav</strong>:{' '}
-            <Link href="mailto:gaurav@example.com">gaurav@example.com</Link>
-          </ListItem>
-        </List>
+        <Box>
+          <Heading as="h2" size="md" mb="2">
+            Our Team
+          </Heading>
+          <List styleType="none" ml="0" mb="4">
+            <ListItem>
+              <strong>Govind</strong>:{' '}
+              <Link href="mailto:govind@example.com">govind@example.com</Link>
+            </ListItem>
+            <ListItem>
+              <strong>Atharv</strong>:{' '}
+              <Link href="mailto:atharv@example.com">atharv@example.com</Link>
+            </ListItem>
+            <ListItem>
+              <strong>Gaurav</strong>:{' '}
+              <Link href="mailto:gaurav@example.com">gaurav@example.com</Link>
+            </ListItem>
+          </List>
+        </Box>
+        <Divider my="4" />
       </Box>
-      <Divider my="4" />
       <footer>
-        <Text>&copy; 2023 YourCompanyName. All rights reserved.</Text>
+        <Text textAlign="center">&copy; 2023 YourCompanyName. All rights reserved.</Text>
       </footer>
     </Box>
   );
